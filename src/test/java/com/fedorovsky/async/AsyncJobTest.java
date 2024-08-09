be05d2ee50e8d01606891b863b07373ca4860eba
Pavel Fedorovskyi <fedorovsky.p@gmail.com>
2024-08-09 15:43:40 +0300

    test exceptionally

--- src/test/java/com/fedorovsky/async/AsyncJobTest.java
+++ src/test/java/com/fedorovsky/async/AsyncJobTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Iterator;
 import java.util.List;
@@ -17,7 +18,7 @@ import java.util.stream.StreamSupport;
 import org.junit.jupiter.api.Test;
 
 public class AsyncJobTest {
-    
+
     @Test
     public void AsyncJobSupport_DoesntConsume() {
         var stream = Stream.of(1, 2, 3);
@@ -37,7 +38,7 @@ public class AsyncJobTest {
     }
 
     @Test
-    public void AsyncJobSupport_ExceptionInUpstream() {
+    public void exceptionInUpstream_stops() throws Exception {
         var iterator = new Iterator<Integer>() {
 
             private AtomicInteger counter = new AtomicInteger(1);
@@ -54,55 +55,111 @@ public class AsyncJobTest {
             public Integer next() {
                 return counter.getAndIncrement();
             }
-            
+
         };
         var stream = StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator, Spliterator.ORDERED), false);
 
-        var out = AsyncJobSupport.of(stream).consume(Function.identity());
+        var job = AsyncJobSupport.of(stream);
+        var out = job.consume(Function.identity());
+
+        assertTrue(job.pr.sharedQueue.isEmpty());
+        assertEquals(List.of(1, 2), out);
+    }
+
+    @Test
+    public void exceptionInDownstream() {
+        var stream = Stream.of(1, 2, 3, 4, 5);
+        var counter = new AtomicInteger();
+
+        var out = AsyncJobSupport.of(stream).andThen(next -> {
+            var el = counter.incrementAndGet();
+            if (el == 3) {
+                throw new RuntimeException("Exception in a 3rd element");
+            }
+            return next;
+        }).consume(Function.identity());
 
         assertEquals(List.of(1, 2), out);
     }
 
     @Test
-    public void AsyncJobSupport_ExceptionInDownstream() {
+    public void exceptionally_Downstream() {
         var stream = Stream.of(1, 2, 3, 4, 5);
         var counter = new AtomicInteger();
+        var holder = new TestUtils.Holder<Throwable>();
 
-        var out = AsyncJobSupport.of(stream)
-            .andThen(next -> {
-                var el = counter.incrementAndGet();
-                if (el == 3) {
-                    throw new RuntimeException("Exception in a 3rd element");
-                }
-                return next;
-            })
-            .consume(Function.identity());
-
-        assertEquals(List.of(1, 2), out);
-    }
-
-    @Test
-    public void AsyncJobSupport_ExceptionallyDownstream() {
-        var stream = Stream.of(1, 2, 3, 4, 5);
-        var counter = new AtomicInteger();
-        var holder = new TestUtils.Holder<Throwable>(); 
-
-        var out = AsyncJobSupport.of(stream)
-            .andThen(next -> {
-                var el = counter.incrementAndGet();
-                if (el == 3) {
-                    throw new RuntimeException("Exception processing the 3rd element");
-                }
-                return next;
-            })
-            .exceptionally(ex -> {
-                holder.set(ex);
-            })
-            .consume(Function.identity());
+        var out = AsyncJobSupport.of(stream).andThen(next -> {
+            var el = counter.incrementAndGet();
+            if (el == 3) {
+                throw new RuntimeException("Exception processing the 3rd element");
+            }
+            return next;
+        }).exceptionally(ex -> {
+            holder.set(ex);
+        }).consume(Function.identity());
 
         assertEquals(List.of(1, 2), out);
         var exception = holder.get();
         assertNotNull(exception);
         assertEquals("Exception processing the 3rd element", exception.getCause().getMessage());
+    }
+
+    @Test
+    public void exceptionally_SharesHandler() {
+        var stream = Stream.of(1, 2, 3, 4, 5);
+        var counter = new AtomicInteger();
+        var holder = new TestUtils.Holder<Throwable>();
+
+        var out = AsyncJobSupport.of(stream).andThen(next -> {
+            var el = counter.incrementAndGet();
+            if (el == 3) {
+                throw new RuntimeException("Exception processing the 3rd element");
+            }
+            return next;
+        }).andThen(i -> i * 2).exceptionally(ex -> {
+            holder.set(ex);
+        }).consume(Function.identity());
+
+        assertEquals(List.of(2, 4), out);
+        var exception = holder.get();
+        assertNotNull(exception);
+        assertEquals("Exception processing the 3rd element", exception.getCause().getMessage());
+    }
+
+    @Test
+    public void exceptionally_OwnHandler() {
+        var stream = Stream.of(1, 2, 3, 4, 5);
+        var counter = new AtomicInteger();
+        var holder = new TestUtils.Holder<Throwable>();
+        var holder2 = new TestUtils.Holder<Throwable>();
+
+        var out = AsyncJobSupport.of(stream).andThen(next -> {
+            var el = counter.incrementAndGet();
+            if (el == 3) {
+                throw new RuntimeException("Exception processing the 3rd element");
+            }
+            return next;
+        }).exceptionally(holder::set).andThen(i -> {
+            if (i == 1) {
+                return 100;
+            }
+            throw new RuntimeException("Child process failed");
+        }).exceptionally(holder2::set).consume(Function.identity());
+
+        assertEquals(List.of(100), out);
+        var exception = holder.get();
+        assertNotNull(exception);
+        assertEquals("Exception processing the 3rd element", exception.getCause().getMessage());
+        exception = holder2.get();
+        assertNotNull(exception);
+        assertEquals("Child process failed", exception.getCause().getMessage());
+    }
+
+    @Test
+    public void startTwice_throws() {
+        var stream = Stream.of(1, 2, 3);
+        var job = AsyncJobSupport.of(stream);
+        job.consume(Function.identity());
+        assertThrows(AsyncJobException.class, () -> job.consume(Function.identity()));
     }
 }
